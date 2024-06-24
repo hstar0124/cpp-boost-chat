42fe659f6c3b33f4a273bcc3c9b98f88040922cc
hstar0124 <dunjun10@naver.com>
2024-06-24 11:47:12 +0900

    add-api-server

---
+++ ApiServer/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using LoginApiServer.Model;
+
+namespace LoginApiServer.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        UserStatusCode CreateUser(User account);
+        UserStatusCode DeleteUser(User user);
+        User? GetUserFromUserid(string id);
+        UserStatusCode LoginUser(User request);
+        UserStatusCode UpdateUser(User user);
+    }
+}
---
+++ ApiServer/Service/UserReadService.cs
@@ -0,0 +1,52 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using LoginApiServer.Model;
+using LoginApiServer.Repository.Interface;
+using LoginApiServer.Service.Interface;
+
+namespace LoginApiServer.Service
+{
+    public class UserReadService : IUserReadService
+    {
+        private readonly ILogger<UserReadService> _logger;
+        private readonly IUserRepository _userRepository;
+
+        public UserReadService(ILogger<UserReadService> logger, IUserRepository userRepository)
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+        }
+
+        public UserResponse GetUserFromUserid(string id)
+        {
+            var user = _userRepository.GetUserFromUserid(id);
+            
+            if (user == null)
+            {
+                _logger.LogError("An error occurred while getting the User for UserId {UserId}.", id);
+                return new UserResponse
+                {
+                    Status = UserStatusCode.Failure,
+                    Message = "An error occurred while getting the User"
+                };
+            }
+            else
+            {
+                var userResponse = new GetUserResponse
+                {
+                    UserId = user.UserId,
+                    Username = user.Username,
+                    Email = user.Email
+                };
+
+                return new UserResponse
+                {
+                    Status = UserStatusCode.Success,
+                    Message = "User retrieved successfully",
+                    Content = Any.Pack(userResponse)
+                };
+            }
+        }
+
+    }
+}
